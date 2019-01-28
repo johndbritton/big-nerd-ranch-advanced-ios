12a988d3f4d0fe17804cd5513b64959abe05dc37
John Britton <public@johndbritton.com>
2019-01-28 10:42:37 -0500

    animate disappear

--- LayersOfFun/LayersOfFun/ViewController.swift
+++ LayersOfFun/LayersOfFun/ViewController.swift
@@ -8,27 +8,11 @@ class ViewController: UIViewController {
     @IBOutlet var imageHeight: NSLayoutConstraint!
     
     @IBAction func buttonTapped(_ sender: UIButton) {
-        
-        if topPin.isActive {
-            imageHeight.constant = imageView.frame.height
-            imageHeight.isActive = true
-            topPin.isActive = false
-        }
-        
-        let params = UISpringTimingParameters(mass: 0.5,
-                                              stiffness: 50.0,
-                                              damping: 1.0,
-                                              initialVelocity: CGVector.zero)
-        
-        let animator = UIViewPropertyAnimator(duration: 1.0, timingParameters: params)
-
-        animator.addAnimations {
-            self.imageHeight.constant -= 100
-            self.view.layoutIfNeeded()
-        }
-        
-        animator.startAnimation()
-        
+        let layer = imageView.layer
+        let anim: CABasicAnimation = CABasicAnimation(keyPath: "opacity")
+        anim.toValue = 0.0
+        anim.duration = 2.0
+        layer.add(anim, forKey: "disappear")
     }
     
     override func viewDidLoad() {
